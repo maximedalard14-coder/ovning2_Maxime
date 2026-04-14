4272aa53f75f7b75c1e392b9038c99656a0e3bf0
maximedalard14-coder <maximedalard14@gmail.com>
2026-04-14 08:46:23 +0200

    Add equals and hashCode methods
    Implement equals and hashCode methods for Recording class.

--- src/Recording.java
+++ src/Recording.java
@@ -35,6 +35,23 @@ public class Recording {
 	public int getYear() {
 		return year;
 	}
+	    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Recording)) {
+            return false;
+        }
+        Recording other = (Recording) o;
+        if (other.title.equals(this.title) && other.artist.equals(this.artist) && other.year == this.year)
+          return true;
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, artist, year);
+    }
 
 	@Override
 	public String toString() {
